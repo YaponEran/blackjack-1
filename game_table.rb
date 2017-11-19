f50f8ae92144476e10c941fdf412ea95a8865ae1
Алексей Мехоношин <ruskidecko@gmail.com>
2017-11-19 23:19:37 +0300

    Игровой стол

--- game_table.rb
+++ game_table.rb
@@ -6,17 +6,25 @@ class GameTable
   attr_reader :players, :bank, :points_to_win, :deck, :value_bet
 
   def initialize(players, deck)
+    cards_start_count = 2
+
     @players = players
     @deck = deck
     @points_to_win = 21
 
-    2.times do |_|
-      @players.each do |player|
-        player.add_card(deck)
-      end
-    end    
+    give_out_cards(cards_start_count)
 
     @value_bet = 10    
     @bank = nil
+  end
+
+  private
+  
+  def give_out_cards(n)
+    players.each do |player|
+      n.times do |_|
+        player.add_card(deck)
+      end
+    end    
   end
 end
