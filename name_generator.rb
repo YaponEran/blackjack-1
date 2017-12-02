1dfb368c39759bc3e3cad4666bf8691da626f1bd
Алексей Мехоношин <ruskidecko@gmail.com>
2017-12-02 17:20:19 +0300

    Game Station - логика

--- name_generator.rb
+++
@@ -1,8 +0,0 @@
-module NameGenerator
-  PREFIXES = %w[jo mo kra zido angu fro qwe].freeze
-  POSTFIXES = %w[rty pod sty zai vic nov rain].freeze
-
-  def generate_name
-    "#{PREFIXES.sample.capitalize}#{POSTFIXES.sample}"
-  end
-end
